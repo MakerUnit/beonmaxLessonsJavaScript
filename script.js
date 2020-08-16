28225b2c7214579bb1bedfd8f96ac1d1accefdc4
Valery <makerunit@gmail.com>
2020-08-16 20:16:03 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+console.log('second' * 9);
+
+// let answear = confirm("Are you here?");
+// console.log(answear);
+
+// let answear = prompt("Есть ли вам 18?", "YeS");
+// console.log(typeof(answear));
+
+let incr = 10, 
+    decr = 10;
+
+incr++;
+decr--;
+console.log(incr, decr);
